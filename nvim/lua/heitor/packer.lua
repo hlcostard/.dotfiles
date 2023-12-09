0c3886b1b43646c5adf38541706cdb6e3ed4c321
Heitor Costard <hlcostard@gmail.com>
2023-12-08 22:11:56 -0300

    Add tmux pane resize shortcut, nvim harpoon2

--- nvim/lua/heitor/packer.lua
+++ nvim/lua/heitor/packer.lua
@@ -13,7 +13,6 @@ return require('packer').startup(function(use)
         requires = { { 'nvim-lua/plenary.nvim'} }
     }
 
-    -- use('tomasiser/vim-code-dark')
     use({ 'rose-pine/neovim', as = 'rose-pine' })
 
     use {
@@ -24,7 +23,13 @@ return require('packer').startup(function(use)
         end,}
 
         use('nvim-treesitter/playground')
-        use('theprimeagen/harpoon')
+
+        use{
+          'theprimeagen/harpoon',
+          branch = 'harpoon2',
+          requires = { { 'nvim-lua/plenary.nvim'} }
+        }
+
         use('tpope/vim-fugitive')
         use('nvim-treesitter/nvim-treesitter-context')
 
@@ -45,30 +50,6 @@ return require('packer').startup(function(use)
             {'L3MON4D3/LuaSnip'},
           }
         }
-
-
-        -- use {
-        --     'VonHeikemen/lsp-zero.nvim',
-        --     branch = 'v1.x',
-        --     requires = {
-        --         -- LSP Support
-        --         {'neovim/nvim-lspconfig'},
-        --         {'williamboman/mason.nvim'},
-        --         {'williamboman/mason-lspconfig.nvim'},
-        --
-        --         -- Autocompletion
-        --         {'hrsh7th/nvim-cmp'},
-        --         {'hrsh7th/cmp-buffer'},
-        --         {'hrsh7th/cmp-path'},
-        --         {'saadparwaiz1/cmp_luasnip'},
-        --         {'hrsh7th/cmp-nvim-lsp'},
-        --         {'hrsh7th/cmp-nvim-lua'},
-        --
-        --         -- Snippets
-        --         {'L3MON4D3/LuaSnip'},
-        --         {'rafamadriz/friendly-snippets'},
-        --     }
-        -- }
 
         use('freddiehaddad/feline.nvim')
         use('nvim-tree/nvim-web-devicons')
